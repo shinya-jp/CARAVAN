1e999179a95baa74e4da3dccde91aa7bf952f65c
shinya-jp <shinya0733@gmail.com>
2020-04-11 18:42:12 +0900

    [Finish]4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,19 @@
 class BlogsController < ApplicationController
   def index
+  	@blogs = Blog.all
   end
 
   def show
   end
 
   def new
+  	@blog = Blog.new
+  end
+
+  def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blgs_path
   end
 
   def edit
